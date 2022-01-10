0e0fdbfc3e3a9c6eca258a83f06c0260d1c0ec76
Gavin <gsmoran1020@protonmail.com>
2022-01-10 23:35:44 +0000

    Updated conversation model to allow for more than 2 users

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -6,23 +6,37 @@ from .user import User
 
 
 class Conversation(utils.CustomModel):
-
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
+    # conversationSignature exists as a secondary identifier for use
+    # in the find_conversation method when the ID is unknown and we need
+    # to find a conversation amongst potentially similar group conversations.
+    conversationSignature = models.TextField(null=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, db_column="userId", related_name="+"
     )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    )
+    otherUsers = models.ManyToManyField(User)
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
+    # find conversation given primary userId and signature
+    def find_conversation(userId, conversationSignature):
         # return conversation or None if it doesn't exist
         try:
             return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
+                (Q(user__id=userId) & Q(conversationSignature__exact=conversationSignature))
             )
         except Conversation.DoesNotExist:
             return None
+
+    
+    # add otherUser to conversation
+    def add_other_user(convoId, other_userId):
+        # return True or False if addition was unsuccessful
+        try:
+            convo = Conversation.objects.get(pk=convoId)
+            other_user = User.objects.get(pk=other_userId)
+            if other_user not in convo.otherUsers.all():
+                convo.otherUsers.add(other_user)
+                return True
+            return False
+        except:
+            return False
